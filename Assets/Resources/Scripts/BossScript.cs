19021e83ca55d59af85ddbf3221cb9a67f5ca3e2
dlobosco <davidlobosco@gmail.com>
2017-04-20 22:51:29 -0400

    update readme, add walk animation and nav mesh

---
+++ Assets/Resources/Scripts/BossScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class BossScript : MonoBehaviour {
+
+	public Transform target;
+	NavMeshAgent agent;
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		GetAllComponents ();
+		WalkToPosition (target.position);
+
+	}
+
+	void GetAllComponents () {
+		agent = GetComponent<NavMeshAgent> ();
+		animator = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void WalkToPosition(Vector3 newPosition) {
+		animator.SetTrigger ("isWalking");
+		agent.SetDestination (newPosition);
+	}
+
+
+}
--- README.md
+++ README.md
@@ -10,3 +10,5 @@ Spring 2017
 - [Mixamo](https://www.mixamo.com): "The Boss" and "Douglas" Models
 
 - [Unity](https://unity3d.com/learn/tutorials/topics/navigation/navmesh-agent): Basic Navigation Script
+
+- [Unity Reference](https://docs.unity3d.com/Manual)
